bb76f76cd2c56ad0977153af6441948e225546de
Eduardo Ferreira <almeifaferreira@gmail.com>
2024-08-07 20:00:32 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 //SERVER COMPONENTS
 // Não consegue adicionar iteratividade ou lógica de negócios
 
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 const Home = () => {
-  return <Button>test</Button>
+  return <div>
+
+   <Header />
+  <div className="p-5">
+    <h2 className="text-xl font-bold">Olá, Eduardo!</h2>
+    <p>Segunda-feira, 05 de agosto</p>
+    <div className="flex items-center gap-2 mt-6">
+      <Input placeholder = "Faça sua busca..."/>
+      <Button size="icon">
+        <SearchIcon />
+      </Button>
+    </div>
+    <div className="relative w-full h-[150px] mt-6">
+      <Image alt="Agende nos melhores FSW Barber" src="/banner-01.png" fill className="object-cover rounded-xl"/>
+    </div>
+
+    </div>
+  </div>
 };
 
 
